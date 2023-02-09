e0190f437bc5cef1b0c203a7872cbccd86456e44
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2023-02-09 16:31:00 +0000

    first 9 rounds for FM, and next 9 rounds for SM. To avoid the order effects

--- second_project/ASPD/__init__.py
+++ second_project/ASPD/__init__.py
@@ -9,7 +9,7 @@ ASPD
 class C(BaseConstants):
     NAME_IN_URL = 'ASPD'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 9
+    NUM_ROUNDS = 18
     payoff_R1 = [350, 350, 350, 350, 350, 350, 350, 350, 350]
     payoff_S1 = [150, 150, 150, 150, 150, 150, 150, 150, 150]
     payoff_T1 = [600, 600, 600, 600, 360, 360, 360, 600, 600]
@@ -23,7 +23,8 @@ class Subsession(BaseSubsession):
     pass
 
 class Group(BaseGroup):
-    task_number = models.IntegerField()
+    task1_number = models.IntegerField()
+    task2_number = models.IntegerField()
     selected_round = models.IntegerField()
     selected_task_be = models.IntegerField()
     selected_round_be = models.IntegerField()
@@ -59,7 +60,8 @@ class Player(BasePlayer):
         ]
     )
 
-    task_number = models.IntegerField()
+    task1_number = models.IntegerField()
+    task2_number = models.IntegerField()
     selected_round = models.IntegerField()
     selected_task_be = models.IntegerField()
     selected_round_be = models.IntegerField()
@@ -70,28 +72,34 @@ def creating_session(subsession: Subsession):
             game_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
             random.shuffle(game_numbers)
             k=0
-            for i in range(C.NUM_ROUNDS):
-                g.in_round(i+1).task_number = game_numbers[i]
+            for i in range(9):
+                g.in_round(i+1).task1_number = game_numbers[i]
                 k=k+1
                 for p in g.get_players():
-                    p.in_round(i+1).task_number = g.in_round(i+1).task_number
+                    p.in_round(i+1).task1_number = g.in_round(i+1).task1_number
+            for i in range(9):
+                g.in_round(i+10).task2_number = game_numbers[i]
+                k=k+1
+                for p in g.get_players():
+                    p.in_round(i+10).task2_number = g.in_round(i+10).task2_number
+
 
         for g in subsession.get_groups():
-            random_round = random.randint(1, C.NUM_ROUNDS)
-            g.in_round(9).selected_round = random_round
+            random_round = random.randint(1, 9)
+            g.in_round(18).selected_round = random_round
             for p in g.get_players():
-                p.in_round(9).selected_round = g.in_round(9).selected_round
+                p.in_round(18).selected_round = g.in_round(18).selected_round
 
         for g in subsession.get_groups():
             task = [0, 1, 2, 3, 4]
             random_task = random.choice(task)
-            g.in_round(9).selected_task_be = random_task
+            g.in_round(18).selected_task_be = random_task
             for p in g.get_players():
-                p.in_round(9).selected_task_be = g.in_round(9).selected_task_be
+                p.in_round(18).selected_task_be = g.in_round(18).selected_task_be
 
-            for i in range(1, C.NUM_ROUNDS + 1):
-                if g.in_round(i).task_number == g.in_round(9).selected_task_be:
-                    g.in_round(9).selected_round_be = i
+            for i in range(1, 9):
+                if g.in_round(i).task1_number == g.in_round(18).selected_task_be:
+                    g.in_round(18).selected_round_be = i
                     break
 
 class ASPD_GamePage_1st(Page):
@@ -99,17 +107,21 @@ class ASPD_GamePage_1st(Page):
     form_fields = ['choice_1st']
 
     @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number <= 9
+
+    @staticmethod
     def vars_for_template(player):
-        task_number = player.task_number
+        task1_number = player.task1_number
         return dict(
-            R1 = C.payoff_R1[task_number],
-            S1 = C.payoff_S1[task_number],
-            T1 = C.payoff_T1[task_number],
-            D1 = C.payoff_D1[task_number],
-            R2 = C.payoff_R2[task_number],
-            S2 = C.payoff_S2[task_number],
-            T2 = C.payoff_T2[task_number],
-            D2 = C.payoff_D2[task_number]
+            R1 = C.payoff_R1[task1_number],
+            S1 = C.payoff_S1[task1_number],
+            T1 = C.payoff_T1[task1_number],
+            D1 = C.payoff_D1[task1_number],
+            R2 = C.payoff_R2[task1_number],
+            S2 = C.payoff_S2[task1_number],
+            T2 = C.payoff_T2[task1_number],
+            D2 = C.payoff_D2[task1_number]
             )
 
 class ASPD_GamePage_2nd(Page):
@@ -117,17 +129,21 @@ class ASPD_GamePage_2nd(Page):
     form_fields = ['choice_2nd_Down', 'choice_2nd_Top']
 
     @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number >= 10
+
+    @staticmethod
     def vars_for_template(player):
-        task_number = player.task_number
+        task2_number = player.task2_number
         return dict(
-            R1=C.payoff_R1[task_number],
-            S1=C.payoff_S1[task_number],
-            T1=C.payoff_T1[task_number],
-            D1=C.payoff_D1[task_number],
-            R2=C.payoff_R2[task_number],
-            S2=C.payoff_S2[task_number],
-            T2=C.payoff_T2[task_number],
-            D2=C.payoff_D2[task_number]
+            R1=C.payoff_R1[task2_number],
+            S1=C.payoff_S1[task2_number],
+            T1=C.payoff_T1[task2_number],
+            D1=C.payoff_D1[task2_number],
+            R2=C.payoff_R2[task2_number],
+            S2=C.payoff_S2[task2_number],
+            T2=C.payoff_T2[task2_number],
+            D2=C.payoff_D2[task2_number]
             )
 
 class ASPD_Instructions(Page):
@@ -170,14 +186,16 @@ class ResultsWaitPage(WaitPage):
         selected_round_be = group.selected_round_be
         selected_task = group.selected_task_be
         group_in_selected_round = group.in_round(selected_round)
-        selected_payment = group_in_selected_round.task_number
+        selected_payment = group_in_selected_round.task1_number
         player_lists = group.get_players()
         player_1 = player_lists[0]
         player_2 = player_lists[1]
-        player_1_in_selected_round_be = player_1.in_round(selected_round_be)
-        player_2_in_selected_round_be = player_2.in_round(selected_round_be)
+        player_1_in_selected_round_be_1st = player_1.in_round(selected_round_be)
+        player_1_in_selected_round_be_2nd = player_1.in_round(selected_round_be+9)
+        player_2_in_selected_round_be_1st = player_2.in_round(selected_round_be)
+        player_2_in_selected_round_be_2nd = player_2.in_round(selected_round_be + 9)
         player_1_in_selected_round = player_1.in_round(selected_round)
-        player_2_in_selected_round = player_2.in_round(selected_round)
+        player_2_in_selected_round = player_2.in_round(selected_round+9)
         if player_1_in_selected_round.choice_1st:
             if player_2_in_selected_round.choice_2nd_Top:
                 player_1.payoff = C.payoff_R1[selected_payment]
@@ -192,8 +210,8 @@ class ResultsWaitPage(WaitPage):
             else:
                 player_1.payoff = C.payoff_D1[selected_payment]
                 player_2.payoff = C.payoff_D2[selected_payment]
-        player_1.participant.vars[__name__] = [str(player_1.payoff), 'First mover', str(selected_round), str(player_2_in_selected_round_be.choice_1st), str(player_2_in_selected_round_be.choice_2nd_Top), str(player_2_in_selected_round_be.choice_2nd_Down), str(selected_task)]
-        player_2.participant.vars[__name__] = [str(player_2.payoff), 'Second mover', str(selected_round), str(player_1_in_selected_round_be.choice_1st), str(player_1_in_selected_round_be.choice_2nd_Top), str(player_1_in_selected_round_be.choice_2nd_Down), str(selected_task)]
+        player_1.participant.vars[__name__] = [str(player_1.payoff), 'First mover', str(selected_round), str(player_2_in_selected_round_be_1st.choice_1st), str(player_2_in_selected_round_be_2nd.choice_2nd_Top), str(player_2_in_selected_round_be_2nd.choice_2nd_Down), str(selected_task)]
+        player_2.participant.vars[__name__] = [str(player_2.payoff), 'Second mover', str(selected_round+9), str(player_1_in_selected_round_be_1st.choice_1st), str(player_1_in_selected_round_be_2nd.choice_2nd_Top), str(player_1_in_selected_round_be_2nd.choice_2nd_Down), str(selected_task)]
 
 
 
