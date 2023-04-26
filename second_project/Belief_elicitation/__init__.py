d626c77a954d576f639cbb018e4f08ec92a0ad75
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2023-04-26 11:12:08 +0100

    BE updating

--- second_project/Belief_elicitation/__init__.py
+++ second_project/Belief_elicitation/__init__.py
@@ -19,6 +19,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
+    timeSpent = models.FloatField()
 
     f1 = models.IntegerField(
         widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
@@ -38,14 +39,28 @@ class Player(BasePlayer):
     f6 = models.IntegerField(
         widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
     )
+    f7 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f8 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f9 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f10 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
 
 class Second_Mover_cooperate(Page):
     form_model = 'player'
-    form_fields = ['f1', 'f2', 'f3']
+    form_fields = ['f1', 'f2', 'f3', 'f4', 'f5','timeSpent']
+    timer_text = 'Time remaining:'
 
 class Second_Mover_defect(Page):
     form_model = 'player'
-    form_fields = ['f4', 'f5', 'f6']
+    form_fields = ['f6', 'f7', 'f8', 'f9', 'f10','timeSpent']
+    timer_text = 'Time remaining:'
 
 
 page_sequence = [Second_Mover_cooperate, Second_Mover_defect,]
