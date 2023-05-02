104979babbc39ee21b2a645387d4e4e4f226fb13
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2023-05-02 16:01:52 +0100

    changes to parameters and BE

--- second_project/Belief_elicitation/__init__.py
+++ second_project/Belief_elicitation/__init__.py
@@ -51,16 +51,44 @@ class Player(BasePlayer):
     f10 = models.IntegerField(
         widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
     )
+    f11 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f12 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f13 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f14 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f15 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
+    f16 = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
+    )
 
-class Second_Mover_cooperate(Page):
+
+class monotone1(Page):
     form_model = 'player'
-    form_fields = ['f1', 'f2', 'f3', 'f4', 'f5','timeSpent']
+    form_fields = ['f1', 'f2', 'f3', 'f4', 'timeSpent']
     timer_text = 'Time remaining:'
 
-class Second_Mover_defect(Page):
+class monotone2(Page):
     form_model = 'player'
-    form_fields = ['f6', 'f7', 'f8', 'f9', 'f10','timeSpent']
+    form_fields = ['f5', 'f6', 'f7', 'f8', 'timeSpent']
     timer_text = 'Time remaining:'
 
+class reciprocal1(Page):
+    form_model = 'player'
+    form_fields = ['f9', 'f10', 'f11', 'f12', 'timeSpent']
+    timer_text = 'Time remaining:'
 
-page_sequence = [Second_Mover_cooperate, Second_Mover_defect,]
+class reciprocal2(Page):
+    form_model = 'player'
+    form_fields = ['f13', 'f14', 'f15', 'f16', 'timeSpent']
+    timer_text = 'Time remaining:'
+
+page_sequence = [monotone1, monotone2, reciprocal1, reciprocal2]
