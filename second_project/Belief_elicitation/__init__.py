e32c740d4607e87b51af8ea1169ff7f630d859df
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2023-05-16 17:38:25 +0100

    minor changes

--- second_project/Belief_elicitation/__init__.py
+++ second_project/Belief_elicitation/__init__.py
@@ -19,8 +19,10 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    timeSpent = models.FloatField()
-
+    timeSpentm1 = models.FloatField()
+    timeSpentm2 = models.FloatField()
+    timeSpentr1 = models.FloatField()
+    timeSpentr2 = models.FloatField()
     f1 = models.IntegerField(
         widget=widgets.RadioSelect, choices=[(0, "0%-20%"), (1, "21%-40%"), (2, "41%-60%"), (3, "61%-80%"),(4, "81%-100%")],
     )
@@ -73,22 +75,22 @@ class Player(BasePlayer):
 
 class monotone1(Page):
     form_model = 'player'
-    form_fields = ['f1', 'f2', 'f3', 'f4', 'timeSpent']
+    form_fields = ['f1', 'f2', 'f3', 'f4', 'timeSpentm1']
     timer_text = 'Time remaining:'
 
 class monotone2(Page):
     form_model = 'player'
-    form_fields = ['f5', 'f6', 'f7', 'f8', 'timeSpent']
+    form_fields = ['f5', 'f6', 'f7', 'f8', 'timeSpentm2']
     timer_text = 'Time remaining:'
 
 class reciprocal1(Page):
     form_model = 'player'
-    form_fields = ['f9', 'f10', 'f11', 'f12', 'timeSpent']
+    form_fields = ['f9', 'f10', 'f11', 'f12', 'timeSpentr1']
     timer_text = 'Time remaining:'
 
 class reciprocal2(Page):
     form_model = 'player'
-    form_fields = ['f13', 'f14', 'f15', 'f16', 'timeSpent']
+    form_fields = ['f13', 'f14', 'f15', 'f16', 'timeSpentr2']
     timer_text = 'Time remaining:'
 
 page_sequence = [monotone1, monotone2, reciprocal1, reciprocal2]
