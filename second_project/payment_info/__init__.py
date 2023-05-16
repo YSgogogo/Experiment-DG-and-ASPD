8cb99cb11dfc837482010abf7a8753c92ac29c54
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2023-05-16 11:51:17 +0100

    minor changes

--- second_project/payment_info/__init__.py
+++ second_project/payment_info/__init__.py
@@ -29,12 +29,7 @@ class Player(BasePlayer):
     round_to_pay2 = models.StringField()
     role_to_pay2 = models.StringField()
     points_to_pay2 = models.StringField()
-    points_to_pay3 = models.IntegerField()
-    points_to_pay4 = models.IntegerField()
-    points_to_pay = models.StringField()
-    block_to_pay = models.IntegerField()
-    round_to_pay = models.StringField()
-    role_to_pay = models.StringField()
+
     Gender =  models.IntegerField(
         widget=widgets.RadioSelect,
         choices=[
@@ -56,25 +51,9 @@ class Survey(Page):
         player.round_to_pay1 = str(participant.vars['DG'][2])
         player.role_to_pay1 = str(participant.vars['DG'][1])
         player.points_to_pay1 = str(participant.vars['DG'][0])
-        player.points_to_pay3 = int(re.findall('\d+', player.points_to_pay1)[0])
         player.round_to_pay2 = str(participant.vars['ASPD'][2])
         player.role_to_pay2 = str(participant.vars['ASPD'][1])
         player.points_to_pay2 = str(participant.vars['ASPD'][0])
-        player.points_to_pay4 = int(re.findall('\d+', player.points_to_pay2)[0])
-
-
-        block = random.randint(1, 2)
-        player.block_to_pay = block
-        if block == 1:
-            player.points_to_pay = player.points_to_pay1
-            player.payoff = 0 - player.points_to_pay4
-            player.round_to_pay = player.round_to_pay1
-            player.role_to_pay = player.role_to_pay1
-        elif block == 2:
-            player.points_to_pay = player.points_to_pay2
-            player.payoff = 0 - player.points_to_pay3
-            player.round_to_pay = player.round_to_pay2
-            player.role_to_pay = player.role_to_pay2
 
 
 
